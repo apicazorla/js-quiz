dcfaf38ee8cc5f0457884b4162731ba208cd653a
apicazorla <fisicayquimica2.0@gmail.com>
2025-10-05 23:35:46 +0200

    Ok

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var i = 0; 
-var txt = 'Consult you the theoretycal contents at web www.nomenclaturaquimica.com';
+var txt = 'Consult you the theoretical contents at web www.nomenclaturaquimica.com';
 var speed = 50;
 function typeWriter() 
 { 
